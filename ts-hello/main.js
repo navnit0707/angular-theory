c1597407ca8bef12d43ce2d3555c44b3020afbaa
navnit0707 <knavnit81@gmail.com>
2022-09-19 21:21:32 +0530

    variable types

--- ts-hello/main.js
+++ ts-hello/main.js
@@ -1,5 +1,38 @@
-function log(message) {
-    console.log(message);
+// let will convert into var while transpile if let produce any error 
+function doSomething() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log('finally', i);
 }
-var message = " hello world";
-log(message);
+doSomething();
+// in ts if you try to change value underfly it will produce error 
+// and while transpile it will be converted to suitable code 
+var a = 5;
+a = "navnit";
+var a;
+a = 5;
+a = true;
+a = "navnit";
+var b;
+b = 5;
+b = "navnit";
+var c;
+c = "navnit";
+var d;
+d = 5;
+d = " hello";
+d = true;
+var e = [1, 2, 3];
+var f = [1, true, 'a', false];
+var colorRed = 0;
+var colorBlue = 1;
+var colorGreen = 2;
+var color;
+(function (color) {
+    color[color["Red"] = 0] = "Red";
+    color[color["Green"] = 1] = "Green";
+    color[color["Blue"] = 2] = "Blue";
+})(color || (color = {}));
+; //auto indexing under the hood 
+var backgroundColor = color.Blue;
--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -1,7 +1,45 @@
-function log(message){
-    console.log(message);
+
+
+// let will convert into var while transpile if let produce any error 
+function doSomething(){
+    for(let i = 0;i<5;i++){
+        console.log(i);
+    }
+    console.log('finally',i)
 }
+doSomething();
 
-var message= " hello world";
+// in ts if you try to change value underfly it will produce error 
+// and while transpile it will be converted to suitable code 
 
-log(message);
+let a = 5;
+a= "navnit";
+
+let a;
+a = 5;
+a = true;
+a="navnit";
+
+let b: number;
+b= 5;
+b= "navnit";
+
+let c:string;
+c = "navnit";
+
+let d: any;
+d = 5;
+d =" hello";
+d = true;
+
+let e: number[] = [1,2,3];
+let f: any[] = [1,true,'a',false];
+
+const colorRed = 0
+const colorBlue = 1;
+const colorGreen = 2;
+
+enum color {Red, Green ,Blue}; //auto indexing under the hood 
+
+let backgroundColor = color.Blue;
+
